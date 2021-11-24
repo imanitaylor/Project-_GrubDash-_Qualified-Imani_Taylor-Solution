d8a74085508880cbac5ba72c273a27c1e71884d1
Imani Taylor <imanir.taylor@gmail.com>
2021-11-24 11:20:20 -0600

    all route added

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -3,8 +3,8 @@ const controller = require("./orders.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 // TODO: Implement the /orders routes needed to make the tests pass
 
-router.route("/:orderId").get(controller.read).put(controller.update).delete(controller.delete);
-router.route("/").get(controller.list).post(controller.create);
+router.route("/:orderId").get(controller.read).put(controller.update).delete(controller.delete).all(methodNotAllowed);
+router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
 
 
 
